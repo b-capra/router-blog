d7018b53d2e39e4d7b3b3ad4e6b5345a94a7758b
b-capra <caprabmc@gmail.com>
2023-01-10 10:52:27 -0500

    Complete chapter 17

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,12 @@
 const About = () => {
-    return (
-      <main>
-          <h1>About</h1>
-      </main>
-    )
-  }
-  
-  export default About
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        This blog app is a project in the Learn React tutorial series.
+      </p>
+    </main>
+  )
+}
+
+export default About
