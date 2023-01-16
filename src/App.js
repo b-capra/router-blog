6040162293eab029ab6bc6de9e682d58d7f45797
b-capra <caprabmc@gmail.com>
2023-01-16 10:48:28 -0500

    Refactor state management
    Implement Easy-peasy Redux and remove Context API

--- src/App.js
+++ src/App.js
@@ -8,26 +8,36 @@ import EditPost from './components/EditPost'
 import PostPage from './components/PostPage'
 import About from './components/About'
 import Missing from './components/Missing'
-
+import { useEffect } from 'react'
+import useAxiosFetch from './hooks/useAxiosFetch'
 import { Route, Switch } from 'react-router-dom'
-import { DataProvider } from './context/DataContext'
-
+import { useStoreActions } from 'easy-peasy'
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts)
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts')
+
+  useEffect(() => {
+    setPosts(data)
+  }, [data, setPosts])
+
   return (
     <div className="App">
       <Header title={'ReactJS Blog'} />
-      <DataProvider>
       <Nav />
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/">
+          <Home 
+            isLoading={isLoading}
+            fetchError={fetchError}
+          />
+        </Route>
         <Route exact path="/post" component={NewPost} />
         <Route path="/edit/:id" component={EditPost} />
         <Route path="/post/:id" component={PostPage} />
         <Route path="/about" component={About} />
         <Route path="*" component={Missing} />
       </Switch>
-      </DataProvider>
       <Footer />
     </div>
   );
